102bf3e03fe6f7f32bfff5613e5e16a8a4d7bf88
Summers Pittman <secondsun@gmail.com>
2014-12-18 11:45:09 -0500

    updated javadoc

--- aerogear-android-push/src/main/java/org/jboss/aerogear/android/unifiedpush/PushRegistrar.java
+++ aerogear-android-push/src/main/java/org/jboss/aerogear/android/unifiedpush/PushRegistrar.java
@@ -26,7 +26,11 @@ public interface PushRegistrar {
      * Registers a device to a push network and any 3rd party application servers.
      * 
      * @param context Android application context
-     * @param callback a callback.
+     * @param callback If registrations is successful, the callback's
+     * onSuccess method will be called and the device will begin receiving push 
+     * messages.  If registration fails the on onFailure method will be called 
+     * with a Exception as a parameter.  It is the application developer's 
+     * responsibility to resolve the error and repeat registration.
      */
     void register(final Context context, final Callback<Void> callback);
 
@@ -35,7 +39,11 @@ public interface PushRegistrar {
      * Unregisters a device a push network and any party application servers.
      * 
      * @param context Android application context
-     * @param callback a callback.
+     * @param callback The onSuccess method will be called after unregistration.  
+     * The device will not receive further notifications.  If there is an error
+     * onFailure will be called with an appropriate Exception.  It is the 
+     * developer's responsibility to resolve the error and ensure the device is 
+     * properly unregistered.
      */
     void unregister(final Context context, final Callback<Void> callback);
 
